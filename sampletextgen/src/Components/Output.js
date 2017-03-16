72dc650ff4a97f8c434566816e1a31a3496accc6
benjaminsw <benjaminsw@live.com>
2017-03-16 19:59:18 +0700

    added new stuff

---
+++ sampletextgen/src/Components/Output.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+class Output extends Component{
+    constructor(props){
+        suoer(props);
+        this.state = {
+            value: props.value
+        }
+    }
+
+    render(){
+        return{
+            <div className="output"
+            {this.props.value}
+            </div>
+            }
+    }
+}
+
+
+export default Output;
