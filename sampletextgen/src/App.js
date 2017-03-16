4832bec985143efcd8e6911818ffc5159596f54e
benjaminsw <benjaminsw@live.com>
2017-03-16 19:58:49 +0700

    updated app.js

--- sampletextgen/src/App.js
+++ sampletextgen/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import './Component/Output'
+import axios from 'axios';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            paras:4,
+            html:true,
+            text:''
+        }
+    }
+
+    componentWillMount(){
+        this.getSampleText();
+    }
+
+    getSampleText(){
+        axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+        .then((response)=> {
+            this.setState({text:response.date.text}, function(){
+                console.log(this.state);
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
   render() {
     return (
       <div className="App">
-        Hello
+        <Output value={this.state.text}/>
       </div>
     );
   }
