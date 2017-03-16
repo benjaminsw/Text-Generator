622dffeb4dc85e286858d291dce6592cc8b7dd17
benjaminsw <benjaminsw@live.com>
2017-03-17 04:27:28 +0700

    updated Text.js

--- sampletextgen/src/Components/Controls/Text.js
+++ sampletextgen/src/Components/Controls/Text.js
@@ -1,1 +1,28 @@
-// JavaScript source code
+import React, {Component} from 'react';
+class Text extends Component{
+    constructor(props){
+        suoer(props);
+        this.state = {
+            value: props.value
+        }
+    }
+
+    onChange(e){
+        this.setState({value:e.target.value}, function(){
+            this.props.onChange(this.state.value);   
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <input className="form-control" type="number" value={this.state.value} onChange={}this.onChange.bind(this) />
+                </select>
+
+            </div>
+    )
+                }
+                }
+
+
+export default Text;
